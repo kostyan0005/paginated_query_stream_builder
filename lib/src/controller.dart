446b0cf8d4cb47082e7308e0d63898b0b0a642bd
Kostyan0005 <rkd000521@gmail.com>
2022-12-10 22:59:04 +0200

    Add the next page subscription logic

--- lib/src/controller.dart
+++ lib/src/controller.dart
@@ -6,13 +6,8 @@ import 'package:flutter/material.dart';
 import 'repository.dart';
 
 class Controller<T> extends ChangeNotifier {
-  final ScrollController scrollController = ScrollController();
-  final List<StreamSubscription<QuerySnapshot<Map<String, dynamic>>>>
-      _pageSubscriptions = [];
-  final List<T> items = [];
-  final List<String> _itemIds = [];
-
   final double minScrollExtentLeft;
+  final String orderBy;
   final int pageSize;
   final T Function(Map<String, dynamic>) itemFromJson;
   final Repository _repo;
@@ -20,16 +15,28 @@ class Controller<T> extends ChangeNotifier {
   Controller({
     required this.minScrollExtentLeft,
     required Query<Map<String, dynamic>> initialQuery,
-    required String orderBy,
+    required this.orderBy,
     required bool descending,
     required this.pageSize,
     required this.itemFromJson,
-  }) : _repo = Repository(initialQuery, orderBy, descending, pageSize);
+  }) : _repo = Repository(initialQuery, orderBy, descending, pageSize) {
+    _setInitialSubscriptions();
+  }
+
+  final ScrollController scrollController = ScrollController();
+  final List<StreamSubscription<QuerySnapshot<Map<String, dynamic>>>>
+      _pageSubscriptions = [];
+  final List<T> _pageItems = [];
+  final List<T> _newItems = [];
+  final List<String> _pageItemIds = [];
+  final List<String> _newItemIds = [];
 
   dynamic _startAt;
   bool _isLoading = true;
   bool _isEmpty = false;
   StreamSubscription<QuerySnapshot<Map<String, dynamic>>>? _newItemSubscription;
+
+  List<T> get items => [..._newItems, ..._pageItems];
 
   double getCacheExtent(BoxConstraints constraints, Axis scrollDirection) {
     final viewportSize = scrollDirection == Axis.vertical
@@ -38,37 +45,77 @@ class Controller<T> extends ChangeNotifier {
     return viewportSize + minScrollExtentLeft * 2;
   }
 
-  void setInitialSubscriptions() async {
+  void _notifyEmpty() {
+    _isEmpty = true;
+    _isLoading = false;
+    notifyListeners();
+  }
+
+  void _setInitialSubscriptions() async {
     _startAt = await _repo.getInitialOrderByValue();
 
     if (_startAt == null) {
-      _isEmpty = true;
-      _isLoading = false;
-      notifyListeners();
+      _notifyEmpty();
     } else {
-      // todo: subscribe to first old item page
+      _addNextSubscription();
     }
 
-    // todo: subscribe to new items
+    _newItemSubscription = _repo
+        .constructNewItemQuery(_startAt)
+        .snapshots()
+        .listen(_listenToNewItemUpdates);
   }
 
-  void addNextSubscription() {
+  void _addNextSubscription() {
+    _isLoading = true;
+    bool isInitialSnap = true;
+
+    final nextPageSubscription =
+        _repo.constructQuery(_startAt).snapshots().listen((snap) {
+      if (isInitialSnap) {
+        final docs = snap.docs;
+        final items = docs.map((doc) => itemFromJson(doc.data()));
+        _pageItems.addAll(items);
+        _pageItemIds.addAll(docs.map((doc) => doc.id));
+
+        if (docs.length < pageSize) {
+          _notifyEmpty();
+        } else {
+          _startAt = docs.last.data()[orderBy];
+          _isLoading = false;
+          notifyListeners();
+        }
+
+        isInitialSnap = false;
+      } else {
+        for (final change in snap.docChanges) {
+          // Ignore this case, as it is not supported.
+          if (change.type == DocumentChangeType.added) return;
+
+          final itemIndex = _pageItemIds.indexOf(change.doc.id);
+          // This should not happen if the package usage requirements are met.
+          if (itemIndex == -1) return;
+
+          if (change.type == DocumentChangeType.modified) {
+            _pageItems[itemIndex] = itemFromJson(change.doc.data()!);
+          } else {
+            _pageItems.removeAt(itemIndex);
+            _pageItemIds.removeAt(itemIndex);
+          }
+          notifyListeners();
+        }
+      }
+    });
+
+    _pageSubscriptions.add(nextPageSubscription);
+  }
+
+  void _listenToNewItemUpdates(QuerySnapshot<Map<String, dynamic>> snap) {
     // todo: define
   }
 
-  // After the initial portion, check for document changes. If a document gets
-  // deleted, remove it from the list, if it gets added, ignore it, as it's
-  // already present in some other list anyway (exception: first list with new
-  // documents). If it gets updated, update the corresponding list item (if it
-  // is present in the corresponding list, otherwise ignore the update).
-
-  // Maintain a separate list with document ids, corresponding to item positions.
-  // This is needed so that when you want to search for the particular element
-  // after it's updated, you can find its position in the ids list, and then
-  // update the item at the corresponding position of the items list.
-
-  // Mark somehow while loading is happening (the period until the initial
-  // stream snapshot is received).
+  // Note: the loading indicator will always be displayed (as the last element
+  // of the list) until _isEmpty becomes true.
 
   @override
   void dispose() {
@@ -77,8 +124,10 @@ class Controller<T> extends ChangeNotifier {
       subscription.cancel();
     }
     _pageSubscriptions.clear();
-    items.clear();
-    _itemIds.clear();
+    _pageItems.clear();
+    _newItems.clear();
+    _pageItemIds.clear();
+    _newItemIds.clear();
     super.dispose();
   }
 }
